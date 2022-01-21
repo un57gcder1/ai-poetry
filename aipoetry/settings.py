de72505f8f5d6fa34c812f182facd62d756fddfa
un57gcder1 <un57gcder1@gmail.com>
2022-01-21 16:32:07 -0600

    Update settings.py

--- aipoetry/settings.py
+++ aipoetry/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%@dktql4y4@py9p&e0hy@b77a_um2-!0m+ml(9o_#cm9b!#tc5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
